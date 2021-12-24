2f4a33a275bcd26210437143f874a07138aa2dcc
Alexandre Jaloto <alexandre.jaloto@inep.gov.br>
2021-12-24 18:11:25 -0300

    pacotes

--- R/funcao.R
+++ R/funcao.R
@@ -95,17 +95,17 @@ desenhar_cat <- function()
   dev.new()
 
   # Set drawing settings ----------------------------------------------------
-  drawSettings(pageWidth = 5, pageHeight = 5, units = "inches")
+draw::drawSettings(pageWidth = 5, pageHeight = 5, units = "inches")
 
 
-  drawPage()
+  draw::drawPage()
 
   # C -----------------------------------------------------------------------
 
   i = 1.9
   while(i >= .9)
   {
-    drawLine(x = c(i, 1.9),
+    draw::drawLine(x = c(i, 1.9),
              y = c(1 ,1))
 
     date_time<-Sys.time()
@@ -118,7 +118,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 2.6)
   {
-    drawLine(x = c(1, 1),
+    draw::drawLine(x = c(1, 1),
              y = c(1 ,i))
 
     date_time<-Sys.time()
@@ -131,7 +131,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 2)
   {
-    drawLine(x = c(1, i),
+    draw::drawLine(x = c(1, i),
              y = c(2.5 ,2.5))
 
     date_time<-Sys.time()
@@ -149,7 +149,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 2.6)
   {
-    drawLine(x = c(2, 2),
+    draw::drawLine(x = c(2, 2),
              y = c(1, i))
 
     date_time<-Sys.time()
@@ -162,7 +162,7 @@ desenhar_cat <- function()
   i = 2
   while(i <= 3)
   {
-    drawLine(x = c(2, i),
+    draw::drawLine(x = c(2, i),
              y = c(2.5, 2.5))
 
     date_time<-Sys.time()
@@ -175,7 +175,7 @@ desenhar_cat <- function()
   i = 2.5
   while(i >= .9)
   {
-    drawLine(x = c(2.9, 2.9),
+    draw::drawLine(x = c(2.9, 2.9),
              y = c(2.5, i))
 
     date_time<-Sys.time()
@@ -188,7 +188,7 @@ desenhar_cat <- function()
   i = 2
   while(i <= 3)
   {
-    drawLine(x = c(2, i),
+    draw::drawLine(x = c(2, i),
              y = c(1.75 ,1.75))
 
     date_time<-Sys.time()
@@ -204,7 +204,7 @@ desenhar_cat <- function()
   i = 3
   while(i <= 4)
   {
-    drawLine(x = c(3, i),
+    draw::drawLine(x = c(3, i),
              y = c(2.5 ,2.5))
 
     date_time<-Sys.time()
@@ -217,7 +217,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 2.6)
   {
-    drawLine(x = c(3.45, 3.45),
+    draw::drawLine(x = c(3.45, 3.45),
              y = c(1 ,i))
 
     date_time<-Sys.time()
@@ -234,7 +234,7 @@ desenhar_cat <- function()
 
   for (i in 1:10)
   {
-    drawLine(x = c(4, seq (4, 4.2, length.out = 10)[i]),
+    draw::drawLine(x = c(4, seq (4, 4.2, length.out = 10)[i]),
              y = c(1.5, seq (1.5, 1, length.out = 10)[i]))
     date_time<-Sys.time()
     while((as.numeric(Sys.time()) - as.numeric(date_time))<.1){} #dummy while loop
@@ -243,7 +243,7 @@ desenhar_cat <- function()
 
   for (i in 1:10)
   {
-    drawLine(x = c(4.2, seq (4.2, 4.4, length.out = 10)[i]),
+    draw::drawLine(x = c(4.2, seq (4.2, 4.4, length.out = 10)[i]),
              y = c(1, seq (1, 1.5, length.out = 10)[i]))
     date_time<-Sys.time()
     while((as.numeric(Sys.time()) - as.numeric(date_time))<.1){} #dummy while loop
@@ -257,7 +257,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 1.6)
   {
-    drawLine(x = c(4.5, 4.5),
+    draw::drawLine(x = c(4.5, 4.5),
              y = c(1, i))
 
     date_time<-Sys.time()
@@ -270,7 +270,7 @@ desenhar_cat <- function()
   i = 4.5
   while(i <= 4.9)
   {
-    drawLine(x = c(4.5, i),
+    draw::drawLine(x = c(4.5, i),
              y = c(1.5 ,1.5))
 
     date_time<-Sys.time()
@@ -283,7 +283,7 @@ desenhar_cat <- function()
   i = 1.4
   while(i >= 0.9)
   {
-    drawLine(x = c(4.9, 4.9),
+    draw::drawLine(x = c(4.9, 4.9),
              y = c(1.5, i))
 
     date_time<-Sys.time()
@@ -296,7 +296,7 @@ desenhar_cat <- function()
   i = 4.6
   while(i <= 4.9)
   {
-    drawLine(x = c(4.5, i),
+    draw::drawLine(x = c(4.5, i),
              y = c(1.25 ,1.25))
 
     date_time<-Sys.time()
@@ -314,7 +314,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 1.6)
   {
-    drawLine(x = c(5, 5),
+    draw::drawLine(x = c(5, 5),
              y = c(1, i))
 
     date_time<-Sys.time()
@@ -326,7 +326,7 @@ desenhar_cat <- function()
 
   for (i in 1:10)
   {
-    drawLine(x = c(5, seq (5, 5.4, length.out = 10)[i]),
+    draw::drawLine(x = c(5, seq (5, 5.4, length.out = 10)[i]),
              y = c(1.5, seq (1.5, 1, length.out = 10)[i]))
     date_time<-Sys.time()
     while((as.numeric(Sys.time()) - as.numeric(date_time))<.1){} #dummy while loop
@@ -336,7 +336,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 1.6)
   {
-    drawLine(x = c(5.4, 5.4),
+    draw::drawLine(x = c(5.4, 5.4),
              y = c(1, i))
 
     date_time<-Sys.time()
@@ -354,7 +354,7 @@ desenhar_cat <- function()
   i = 5.6
   while(i <= 5.9)
   {
-    drawLine(x = c(5.5, i),
+    draw::drawLine(x = c(5.5, i),
              y = c(1.5, 1.5))
 
     date_time<-Sys.time()
@@ -367,7 +367,7 @@ desenhar_cat <- function()
   i = 1
   while(i <= 1.6)
   {
-    drawLine(x = c(5.7, 5.7),
+    draw::drawLine(x = c(5.7, 5.7),
              y = c(1 ,i))
 
     date_time<-Sys.time()
@@ -383,7 +383,7 @@ desenhar_cat <- function()
   i = 1.1
   while(i <= 1.6)
   {
-    drawLine(x = c(6, 6),
+    draw::drawLine(x = c(6, 6),
              y = c(1, i))
 
     date_time<-Sys.time()
@@ -396,7 +396,7 @@ desenhar_cat <- function()
   i = 6.1
   while(i <= 6.4)
   {
-    drawLine(x = c(6, i),
+    draw::drawLine(x = c(6, i),
              y = c(1.5 ,1.5))
 
     date_time<-Sys.time()
@@ -409,7 +409,7 @@ desenhar_cat <- function()
   i = 6.1
   while(i <= 6.4)
   {
-    drawLine(x = c(6, i),
+    draw::drawLine(x = c(6, i),
              y = c(1.25 ,1.25))
 
     date_time<-Sys.time()
@@ -422,7 +422,7 @@ desenhar_cat <- function()
   i = 6.1
   while(i <= 6.4)
   {
-    drawLine(x = c(6, i),
+    draw::drawLine(x = c(6, i),
              y = c(1 ,1))
 
     date_time<-Sys.time()
